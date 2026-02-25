f25572a2dfb7b5a5c4824ff96f6e568dd20a3345
Semere M. Mebrahtom <semere.meharena1@gmail.com>
2026-02-25 13:25:53 +0100

    all done

---
+++ ex4/ft_raise_errors.py
@@ -0,0 +1,38 @@
+def check_plant_health(plant_name: str, water_level: int, sunlight_hours: int)\
+                        -> str:
+    if plant_name == "":
+        raise Exception("Plant name cannot be empty")
+    if not 1 <= water_level <= 10:
+        raise Exception(
+            f"Water level {water_level} is too " +
+            f"{'high' if water_level > 10 else 'low'} (1 - 10)"
+        )
+    if not 2 <= sunlight_hours <= 12:
+        raise Exception(
+            f"Sunlight hours {sunlight_hours} is too " +
+            f"{'high' if sunlight_hours > 12 else 'low'} (2 - 12)"
+        )
+    return f"Plant {plant_name} is healthy!"
+
+
+def test_plant_checks() -> None:
+    plants = [
+        ("Testing  good values", ("tomato", 7, 7)),
+        ("Testing empty plant name", ("", 7, 7)),
+        ("Testing bad water level...", ("tomato", 15, 7)),
+        ("Testing bad sunlight hours...", ("tomato", 7, 0)),
+    ]
+    print("=== Garden Plant Health Checker ===")
+    for plant in plants:
+        print("\n" + plant[0])
+        try:
+            res = check_plant_health(*plant[1])
+        except Exception as e:
+            print(f"Error: {e}")
+        else:
+            print(res)
+    print("\nAll error raising tests completed!")
+
+
+if __name__ == "__main__":
+    test_plant_checks()
---
+++ ex5/ft_garden_management.py
@@ -0,0 +1,116 @@
+class GardenManager:
+    def __init__(self, tank_level: int) -> None:
+        self.tank_level = tank_level
+        self.plants = []
+
+    class GardenException(Exception):
+        def __init__(self, msg: str) -> None:
+            super().__init__(msg)
+
+    class Plant:
+        def __init__(
+            self,
+            name: str,
+            water_level: int,
+            water_cost: int,
+            sunlight_hours: int,
+        ) -> None:
+            if name == "":
+                raise Exception("Error adding plant: Plant name cannot be "
+                                + "empty!")
+            self.name = name
+            self.water_level = water_level
+            self.water_consumption = water_cost
+            self.sunlight_hours = sunlight_hours
+            print(f"Added {name} successfylly")
+
+        def water_plant(self, tank_level: int) -> int:
+            if tank_level < self.water_consumption:
+                raise Exception(f"Not enough water in tank for {self.name}")
+            print(f"Watering {self.name} - success")
+            return tank_level - self.water_consumption
+
+        def check_health(self) -> None:
+            if not 1 <= self.water_level <= 10:
+                raise Exception(
+                    f"Error checking {self.name}: "
+                    + f"Water level {self.water_level} is too "
+                    + f"{'high' if self.water_level > 10 else 'low'} (1 - 10)"
+                )
+            if not 2 <= self.sunlight_hours <= 12:
+                raise Exception(
+                    f"Error checking {self.name}: "
+                    + f"Sunlight hours {self.sunlight_hours} is too "
+                    + f"{'high' if self.sunlight_hours > 12 else 'low'}"
+                    + "(2 - 12)"
+                )
+            print(
+                f"{self.name}: healthy (water: {self.water_level}, "
+                + f"sun: {self.sunlight_hours})"
+            )
+
+    def add_plant(
+        self, name: str, water_level: int, water_cost: int, sunlight_hours: int
+    ) -> None:
+        self.plants[:0] = [
+            self.Plant(name, water_level, water_cost, sunlight_hours)
+        ]
+
+    def water_plants(self) -> None:
+        for plant in self.plants:
+            self.tank_level = plant.water_plant(self.tank_level)
+
+    def check_plant_health(self) -> None:
+        for plant in self.plants:
+            plant.check_health()
+
+    def check_tank_level(self) -> None:
+        if self.tank_level < 10:
+            raise Exception("GardenError: Not enough water in tank")
+
+
+def test_garden_management() -> None:
+    plants = [
+        ("Cabbage", 5, 15, 24),
+        ("Lettuce", 15, 20, 8),
+        ("Tomato", 5, 30, 8),
+        ("", 5, 15, 8),
+    ]
+    garden_mgr = GardenManager(50)
+
+    print("\nAdding plants to garden...")
+    try:
+        for plant in plants:
+            garden_mgr.add_plant(*plant)
+    except Exception as e:
+        print(e)
+
+    print("\nWatering plants..")
+    try:
+        garden_mgr.water_plants()
+    except Exception as e:
+        print(e)
+    finally:
+        print("Closing watering system (cleanup)")
+
+    print("\nChecking plant health...")
+    try:
+        garden_mgr.check_plant_health()
+    except Exception as e:
+        print(e)
+    finally:
+        print("Stopping health checks")
+
+    print("\nTesting error recovery...")
+    try:
+        garden_mgr.check_tank_level()
+    except Exception:
+        print("Caught Garden Error: Not enough water in tank")
+    finally:
+        print("System recovered and continuing...")
+
+    print("\nGarden management system test complete!")
+
+
+if __name__ == "__main__":
+    test_garden_management()
